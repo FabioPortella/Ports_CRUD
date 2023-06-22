110cf271b973614e505dce815cc5188d513a2fc5
FabioPortella <fabioportella2009@gmail.com>
2023-06-22 19:45:52 -0300

    CRUD Laravel - Pos - Internet I

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> Projeto Laravel</title>
+
+    </head>
+
+    <body class="antialiased">
+
+        <h1> Projeto: Fábio de Abreu Portella </h1>
+        <p> CRUD de um
+            <a href="/books/"> Cadastro de Livros </a>
+        </p>
+    </body>
+</html>
